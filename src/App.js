cb323d1e058db4143eec2efceef1dd5bdd54dafe
Sundar <psundar001@gmail.com>
2020-02-24 09:41:05 +0530

    Added map to the site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
 
 function App() {
+  const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    >
+       <Marker
+      position={{ lat: -34.397, lng: 150.644 }}
+    />
+  </GoogleMap>
+));
+
   return (
-    <div className="App">
-        Trip Advisor V2
+    <div>  
+      <div className="App">Trip Advisor V2 </div>  
+      <MapWithAMarker
+  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBvOjX5Zf6kVsbG2AaPQfz1xxKPYbJ1ViY&v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+/>
     </div>
   );
 }
 
-export default App;
+export default App; 
